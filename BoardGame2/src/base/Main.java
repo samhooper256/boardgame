824ff77cfb23082441f390643e802a9ed1f76be8
innoc <innoc@10.203.0.219>
2021-11-29 12:06:31 -0600

    added a rectangle

--- BoardGame2/src/base/Main.java
+++ BoardGame2/src/base/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -27,6 +28,9 @@ public class Main extends Application {
 		c.setLayoutY(100);
 		
 		sp.getChildren().add(c);
+		
+		Rectangle r = new Rectangle(1, 1, 1, 1);
+		sp.getChildren().add(r);
 	
 		stage.show();
 	}
