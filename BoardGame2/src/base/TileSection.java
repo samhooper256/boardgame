7f5f15d8c8302380d627b605e70538509e2f8d6c
samho <samho@10.203.10.67>
2021-12-01 11:39:24 -0600

    added TileSection.randomOrder()

--- BoardGame2/src/base/TileSection.java
+++ BoardGame2/src/base/TileSection.java
@@ -17,14 +17,23 @@ public class TileSection {
 					new SafeTile(), new MinigameTile(), new MinigameTile())
 	));
 	
-	private final Tile[] tiles;
+	private final List<Tile> tiles;
 	
 	public TileSection(Tile... tiles) {
-		this.tiles = tiles;
+		this.tiles = new ArrayList<>(tiles.length);
+		Collections.addAll(this.tiles, tiles);
 	}
 
-	public Tile[] tiles() {
-		return tiles;
+	/** Returns the {@link Tile Tiles} that comprise this {@link TileSection}. The returned order is the same every
+	 * time. */
+	public List<Tile> tilesUnmodifiable() {
+		return Collections.unmodifiableList(tiles);
+	}
+	
+	public List<Tile> randomOrder() {
+		List<Tile> order = new ArrayList<>(tiles);
+		Collections.shuffle(order);
+		return order;
 	}
 	
 }
