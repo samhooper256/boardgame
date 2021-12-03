5d54103c1bbc62d4bdf252606d2dd4c1113870dd
samho <samho@10.203.10.67>
2021-12-03 11:12:49 -0600

    Started work on Die.

--- BoardGame2/src/base/Die.java
+++ BoardGame2/src/base/Die.java
@@ -1,8 +1,19 @@
 package base;
 
+import java.util.function.*;
+
 import fxutils.Images;
+import javafx.animation.*;
+import javafx.util.Duration;
 
 public class Die extends ImagePane {
+	
+	
+	private static final Duration ROLL_DURATION = Duration.millis(2000);
+	private static final int FACE_COUNT = 10;
+	private static final IntFunction<Duration> DURATION_FUNCTION = i -> {
+		return Duration.millis(FACE_COUNT * Math.sqrt(i) / Math.sqrt(ROLL_DURATION.toMillis()));
+	};
 	
 	public Die() {
 		super(Images.DIE0);
@@ -13,6 +24,18 @@ public class Die extends ImagePane {
 	
 	public void roll() {
 		System.out.println("roll");
+		Timeline t = new Timeline();
+		for(int i = 0; i < FACE_COUNT; i++) {
+			t.getKeyFrames().add(new KeyFrame(DURATION_FUNCTION.apply(i), eh -> {
+				
+			}));
+		}
 	}
+	
+	/** Accepts an {@code int} from {@code 1} to {@code 6} (inclusive). */
+	private void setFace(int face) {
+		
+	}
+	
 
 }
