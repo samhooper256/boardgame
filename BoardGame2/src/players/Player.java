9e1ae7c2644455af9ff9623bb4cc7e87c35d3e19
samho <samho@10.203.10.67>
2021-12-07 10:55:33 -0600

    added temporary player images

---
+++ BoardGame2/src/players/Player.java
@@ -0,0 +1,13 @@
+package players;
+
+import base.ImagePane;
+import javafx.scene.image.Image;
+
+public class Player extends ImagePane {
+
+	public Player(Image image) {
+		super(image);
+		// TODO Auto-generated constructor stub
+	}
+
+}
