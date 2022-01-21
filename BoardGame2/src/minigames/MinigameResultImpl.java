a9a68c2641a956d7788f940f4ed96361dba2f7ee
samhooper256 <samhooper256@gmail.com>
2022-01-20 18:42:54 -0600

    Improved minigames.MinigameResultImpl

--- BoardGame2/src/minigames/MinigameResultImpl.java
+++ BoardGame2/src/minigames/MinigameResultImpl.java
@@ -1,5 +1,28 @@
 package minigames;
 
+import java.util.*;
+
+import medals.MedalReward;
+
 public class MinigameResultImpl {
 
+	/** Unmodifiable. */
+	private final Set<MedalReward> rewards;
+	
+	public MinigameResultImpl(MedalReward... rewards) {
+		Set<MedalReward> set = new HashSet<>();
+		Collections.addAll(set, rewards);
+		this.rewards = Collections.unmodifiableSet(set);
+	}
+	
+	/** The given {@link Collection} is defensively copied, and duplicates are removed. */
+	public MinigameResultImpl(Collection<MedalReward> rewards) {
+		this.rewards = Collections.unmodifiableSet(new HashSet<>(rewards));
+	}
+	
+	/** The returned {@link Set} is unmodifiable. */
+	public Set<MedalReward> rewards() {
+		return rewards;
+	}
+	
 }
