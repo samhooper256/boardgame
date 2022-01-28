59be38adf5ae705a8c66b3ffb7f0e2bf1df379a8
samho <samho@LAPTOP-U6S44RL2.attlocal.net>
2022-01-27 20:35:46 -0600

    Updated to-do list.

--- BoardGame2/src/base/Main.java
+++ BoardGame2/src/base/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 import players.Player;
 
 //TODO:
-// - ring around player whose turn it is.
+// - user can select how many players.
 public class Main extends Application {
 
 	public static final String TITLE = "greek olympics and stuff";
