cb07106efc453083553a32eaa667e09cefb630b6
samhooper256 <samhooper256@gmail.com>
2022-02-06 20:06:48 -0600

    Made game.board.fx.MedalLabel text white.

--- BoardGame2/src/game/board/fx/MedalLabel.java
+++ BoardGame2/src/game/board/fx/MedalLabel.java
@@ -2,12 +2,14 @@ package game.board.fx;
 
 import fxutils.Fonts;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 
 public class MedalLabel extends Label {
 
 	public MedalLabel(int value) {
 		super();
 		setFont(Fonts.UI_MED);
+		setTextFill(Color.WHITE);
 		setValue(value);
 	}
 
