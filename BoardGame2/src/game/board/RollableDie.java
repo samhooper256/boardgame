94fd5ee96474bece69baf63b3c18c866740ec749
samhooper256 <samhooper256@gmail.com>
2022-02-21 08:57:29 -0600

    Notified HelperInfo when a rollable die is rolled.

--- BoardGame2/src/game/board/RollableDie.java
+++ BoardGame2/src/game/board/RollableDie.java
@@ -6,6 +6,7 @@ import java.util.function.*;
 import base.Updatable;
 import base.panes.ImagePane;
 import fxutils.Images;
+import game.helper.HelperInfo;
 import utils.*;
 
 public final class RollableDie extends ImagePane implements Die, Updatable {
@@ -49,6 +50,7 @@ public final class RollableDie extends ImagePane implements Die, Updatable {
 	}
 	
 	private void startRoll() {
+		HelperInfo.get().setRolledRollableDie();
 		rollElapsed = 0;
 		nextSwitchIndex = 1;
 	}
